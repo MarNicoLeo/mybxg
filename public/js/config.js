1af6f91e94fc4cc7b88600abda376c40ce949318
nico <fengxingtianxialiu@gmail.com>
2017-06-07 21:14:46 +0800

    模块化开发 1.0

--- public/js/config.js
+++ public/js/config.js
@@ -1,15 +1,18 @@
+/**
+ * Created by Administrator on 2017/6/7.
+ */
 requirejs.config({
-    baseUrl : '/public/assets',//设置模块加载的基准路径
-    paths : {// 给模块路径起一个别名
-        jquery : 'jquery/jquery.min',
-        bootstrap : 'bootstrap/js/bootstrap.min',
-        cookie : 'jquery-cookie/jquery.cookie',
-        common : '../js/common',
-        login : '../js/login'
+    baseUrl:'/public/assets',
+    paths:{
+        jquery:"jquery/jquery.min",
+        bootstrap:"bootstrap/js/bootstrap.min",
+        cookie:"jquery-cookie/jquery.cookie",
+        login:"../js/login",
+        common:'../js/common'
     },
-    shim : {// 兼容非标准模块
-        bootstrap : {
-            deps : ['jquery']
+    shim:{
+        bootstrap:{
+            deps:['jquery']
         }
     }
-});
+})
--- public/js/login.js
+++ public/js/login.js
@@ -1,20 +1,21 @@
-define(['jquery','cookie'],function($){
-    // 实现登录功能
-    $('#loginId').click(function(){
+/**
+ * Created by Administrator on 2017/6/7.
+ */
+define(['jquery','cookie'],function ($) {
+    $('#loginId').click(function () {
         $.ajax({
-            type : 'post',
-            url : '/api/login',
-            data : $('#loginForm').serialize(),
-            dataType : 'json',
-            success : function(data){
-                if(data.code == 200){
-                    // 把登录的用户信息存储到cookie里面，方便页面之间进行数据共享
+            type:'post',
+            url:'/api/login',
+            dataType:'json',
+            data:$('#loginForm').serialize(),
+            success: function (data) {
+                if(data.code==200){
                     $.cookie('loginInfo',JSON.stringify(data.result),{path:'/'});
-                    // 跳转到主页面
-                    location.href = '/index/index';
+                    console.log('1');
+                    location.href='/index/index';
                 }
             }
         });
         return false;
-    });
-});
+    })
+})
